8c96f1896af04362e410cc9c9a8b0469cdf2ede6
SejongKang <sejong055@gmail.com>
2020-05-05 10:23:56 +0900

    Arrangement

--- Main.py
+++ Main.py
@@ -88,126 +88,126 @@ if __name__ == "__main__" :
     seq_length = 5
     scaler = MinMaxScaler()
 
-    # test_data_size = 14
-    #
-    # train_data = daily_cases[:-test_data_size]
-    # test_data = daily_cases[-test_data_size:]
-    #
-    #
-    # scaler = scaler.fit(np.expand_dims(train_data, axis=1))
-    #
-    # train_data = scaler.transform(np.expand_dims(train_data, axis=1))
-    #
-    # test_data = scaler.transform(np.expand_dims(test_data, axis=1))
-    #
-    #
-    # X_train, y_train = create_sequences(train_data, seq_length)
-    # X_test, y_test = create_sequences(test_data, seq_length)
-    #
-    # X_train = torch.from_numpy(X_train).float()
-    # y_train = torch.from_numpy(y_train).float()
-    #
-    # X_test = torch.from_numpy(X_test).float()
-    # y_test = torch.from_numpy(y_test).float()
-    #
-    # model = LSTM.CoronaVirusPredictor(
-    #     n_features=1,
-    #     n_hidden=512,
-    #     seq_len=seq_length,
-    #     n_layers=2
-    # )
-    #
-    # model, train_hist, test_hist = train_model(model, X_train, y_train, X_test, y_test)
-    #
-    # with torch.no_grad():
-    #   test_seq = X_test[:1]
-    #   preds = []
-    #   for _ in range(len(X_test)):
-    #     y_test_pred = model(test_seq)
-    #     pred = torch.flatten(y_test_pred).item()
-    #     preds.append(pred)
-    #     new_seq = test_seq.numpy().flatten()
-    #     new_seq = np.append(new_seq, [pred])
-    #     new_seq = new_seq[1:]
-    #     test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
-    #
-    # true_cases = scaler.inverse_transform(
-    #     np.expand_dims(y_test.flatten().numpy(), axis=0)
-    # ).flatten()
-    #
-    # predicted_cases = scaler.inverse_transform(
-    #   np.expand_dims(preds, axis=0)
-    # ).flatten()
-    #
-    # plt.plot(
-    #   daily_cases.index[:len(train_data)],
-    #   scaler.inverse_transform(train_data).flatten(),
-    #   label='Historical Daily Cases'
-    # )
-    #
-    # plt.plot(
-    #   daily_cases.index[len(train_data):len(train_data) + len(true_cases)],
-    #   true_cases,
-    #   label='Real Daily Cases'
-    # )
-    #
-    # plt.plot(
-    #   daily_cases.index[len(train_data):len(train_data) + len(true_cases)],
-    #   predicted_cases,
-    #   label='Predicted Daily Cases'
-    # )
-    #
-    # plt.legend()
-    # plt.show()
+    test_data_size = 14
 
-# All Data
-    scaler = scaler.fit(np.expand_dims(daily_cases, axis=1))
+    train_data = daily_cases[:-test_data_size]
+    test_data = daily_cases[-test_data_size:]
 
-    all_data = scaler.transform(np.expand_dims(daily_cases, axis=1))
 
-    X_all, y_all = create_sequences(all_data, seq_length)
+    scaler = scaler.fit(np.expand_dims(train_data, axis=1))
 
-    X_all = torch.from_numpy(X_all).float()
-    y_all = torch.from_numpy(y_all).float()
+    train_data = scaler.transform(np.expand_dims(train_data, axis=1))
+
+    test_data = scaler.transform(np.expand_dims(test_data, axis=1))
+
+
+    X_train, y_train = create_sequences(train_data, seq_length)
+    X_test, y_test = create_sequences(test_data, seq_length)
+
+    X_train = torch.from_numpy(X_train).float()
+    y_train = torch.from_numpy(y_train).float()
+
+    X_test = torch.from_numpy(X_test).float()
+    y_test = torch.from_numpy(y_test).float()
 
     model = LSTM.CoronaVirusPredictor(
         n_features=1,
         n_hidden=512,
         seq_len=seq_length,
-        n_layers=10
+        n_layers=2
     )
-    model, train_hist, _ = train_model(model, X_all, y_all)
 
-    DAYS_TO_PREDICT = 3
+    model, train_hist, test_hist = train_model(model, X_train, y_train, X_test, y_test)
 
     with torch.no_grad():
-        test_seq = X_all[:1]
-        preds = []
-        for _ in range(DAYS_TO_PREDICT):
-            y_test_pred = model(test_seq)
-            pred = torch.flatten(y_test_pred).item()
-            preds.append(pred)
-            new_seq = test_seq.numpy().flatten()
-            new_seq = np.append(new_seq, [pred])
-            new_seq = new_seq[1:]
-            test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
+      test_seq = X_test[:1]
+      preds = []
+      for _ in range(len(X_test)):
+        y_test_pred = model(test_seq)
+        pred = torch.flatten(y_test_pred).item()
+        preds.append(pred)
+        new_seq = test_seq.numpy().flatten()
+        new_seq = np.append(new_seq, [pred])
+        new_seq = new_seq[1:]
+        test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
 
-    predicted_cases = scaler.inverse_transform(
-        np.expand_dims(preds, axis=0)
+    true_cases = scaler.inverse_transform(
+        np.expand_dims(y_test.flatten().numpy(), axis=0)
     ).flatten()
 
-    predicted_index = pd.date_range(
-        start=daily_cases.index[-1],
-        periods=DAYS_TO_PREDICT + 1,
-        closed='right'
+    predicted_cases = scaler.inverse_transform(
+      np.expand_dims(preds, axis=0)
+    ).flatten()
+
+    plt.plot(
+      daily_cases.index[:len(train_data)],
+      scaler.inverse_transform(train_data).flatten(),
+      label='Historical Daily Cases'
     )
 
-    predicted_cases = pd.Series(
-        data=predicted_cases,
-        index=predicted_index
+    plt.plot(
+      daily_cases.index[len(train_data):len(train_data) + len(true_cases)],
+      true_cases,
+      label='Real Daily Cases'
     )
 
-    plt.plot(daily_cases, label='Historical Daily Cases')
-    plt.plot(predicted_cases, label='Predicted Daily Cases')
-    plt.legend();
+    plt.plot(
+      daily_cases.index[len(train_data):len(train_data) + len(true_cases)],
+      predicted_cases,
+      label='Predicted Daily Cases'
+    )
+
+    plt.legend()
     plt.show()
+
+# All Data
+#     scaler = scaler.fit(np.expand_dims(daily_cases, axis=1))
+#
+#     all_data = scaler.transform(np.expand_dims(daily_cases, axis=1))
+#
+#     X_all, y_all = create_sequences(all_data, seq_length)
+#
+#     X_all = torch.from_numpy(X_all).float()
+#     y_all = torch.from_numpy(y_all).float()
+#
+#     model = LSTM.CoronaVirusPredictor(
+#         n_features=1,
+#         n_hidden=512,
+#         seq_len=seq_length,
+#         n_layers=10
+#     )
+#     model, train_hist, _ = train_model(model, X_all, y_all)
+#
+#     DAYS_TO_PREDICT = 3
+#
+#     with torch.no_grad():
+#         test_seq = X_all[:1]
+#         preds = []
+#         for _ in range(DAYS_TO_PREDICT):
+#             y_test_pred = model(test_seq)
+#             pred = torch.flatten(y_test_pred).item()
+#             preds.append(pred)
+#             new_seq = test_seq.numpy().flatten()
+#             new_seq = np.append(new_seq, [pred])
+#             new_seq = new_seq[1:]
+#             test_seq = torch.as_tensor(new_seq).view(1, seq_length, 1).float()
+#
+#     predicted_cases = scaler.inverse_transform(
+#         np.expand_dims(preds, axis=0)
+#     ).flatten()
+#
+#     predicted_index = pd.date_range(
+#         start=daily_cases.index[-1],
+#         periods=DAYS_TO_PREDICT + 1,
+#         closed='right'
+#     )
+#
+#     predicted_cases = pd.Series(
+#         data=predicted_cases,
+#         index=predicted_index
+#     )
+#
+#     plt.plot(daily_cases, label='Historical Daily Cases')
+#     plt.plot(predicted_cases, label='Predicted Daily Cases')
+#     plt.legend();
+#     plt.show()
